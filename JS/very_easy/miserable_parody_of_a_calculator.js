f24844e9126a3ff00b306960e37d0ff9ab6c9289
BigMan <IIAchilles@protonmail.com>
2025-05-05 22:22:07 +0100

    Miserable Parody of a Calculator Solution Added

--- JS/very_easy/miserable_parody_of_a_calculator.js
+++ JS/very_easy/miserable_parody_of_a_calculator.js
@@ -1,9 +1,11 @@
 /*
 Problem: Miserable Parody of a Calculator
 Difficulty: very easy
-Link: 
+Link: https://edabit.com/challenge/fh9i7k936rvfjnCYR
 Date: 2025-05-05
 */
 
-
+function calculator(str) {
+	return eval(str);
+}
 
