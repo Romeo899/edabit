4369dbb6c937035c9515945d0f80759b60367a6c
BigMan <IIAchilles@protonmail.com>
2025-05-06 22:14:33 +0100

    Challenge: buggy code COMPLETE

---
+++ JS/very_easy/buggy_code.js
@@ -0,0 +1,14 @@
+/*
+Problem: Buggy Code
+Difficulty: very easy
+Link: https://edabit.com/challenge/r2MbSxquAGPnDkjQx
+Date: 2025-05-06
+*/
+
+function has_bugs(n) {
+	if (n == true) {
+		return 'sad days';
+	} else if (n == false){
+		return `it's a good day`;
+	}
+}
