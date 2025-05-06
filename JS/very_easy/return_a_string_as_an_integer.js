60274891b3f4fb53fb658a7795e8a66e29dbb1a3
BigMan <IIAchilles@protonmail.com>
2025-05-06 22:17:40 +0100

    Challenge: Return a String as an Integer COMPLETE

---
+++ JS/very_easy/return_a_string_as_an_integer.js
@@ -0,0 +1,11 @@
+/*
+Problem: Return a String as an Integer
+Difficulty: very easy
+Link: https://edabit.com/challenge/rGsgEswWuW339yNxY
+Date: 2025-05-06
+*/
+
+
+function stringInt(str) {
+	return Number(str)
+}
