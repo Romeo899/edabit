b1d969c3f37805d6ec8bcd37500bd09c24bac875
BigMan <IIAchilles@protonmail.com>
2025-05-06 22:03:19 +0100

    Challenge: Using Ternary Operators COMPLETE

---
+++ JS/very_easy/using_ternary_operators.js
@@ -0,0 +1,10 @@
+/*
+Problem: Using Ternary Operators
+Difficulty: very easy
+Link: Using Ternary Operators
+Date: 2025-05-06
+*/
+
+function yeah_nope(bool) {
+	return bool === true ? 'yeah' : 'nope';
+}
