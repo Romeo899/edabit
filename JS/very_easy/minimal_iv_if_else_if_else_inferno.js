12a3909fa5c65848ade197d7a14adde331602e2f
BigMan <IIAchilles@protonmail.com>
2025-05-06 20:07:14 +0100

    Challenge: Minimal IV: if-else if-else Inferno COMPLETE

---
+++ JS/very_easy/minimal_iv_if_else_if_else_inferno.js
@@ -0,0 +1,12 @@
+/*
+Problem: Minimal IV: if-else if-else Inferno
+Difficulty: very easy
+Link: https://edabit.com/challenge/MiLoSm7STCWcrPC3k
+Date: 2025-05-06
+*/
+
+function equilibrium (x) {
+	if (x > 0) return "positive"
+	if (x < 0) return "negative"
+	else return true
+}
