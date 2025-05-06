d2d081d236aa51468d920a9c7bd7dc2e8f53ff6e
BigMan <IIAchilles@protonmail.com>
2025-05-06 22:09:06 +0100

    Challenge: arrow functions COMPLETE

---
+++ JS/very_easy/arrow_functions.js
@@ -0,0 +1,12 @@
+/*
+Problem: Arrow Functions
+Difficulty: very easy
+Link: https://edabit.com/challenge/qcw8QBqa4amN2x4q4
+Date: 2025-05-06
+*/
+
+add2 = (x) => x + 2;
+add3 = (x) => x + 3;
+add5 = (x) => x + 5;
+add7 = (x) => x + 7;
+add11 = (x) => x + 11;
